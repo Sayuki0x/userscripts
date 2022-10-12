4fccd6ec3f407c7d64d8fdcd7b9e2adba6b8550c
ExtraHash <ExtraHash@protonmail.com>
2022-10-12 09:49:51 -0600

    improvements to zapper userscript

--- zapper-fix-staking-balance.js
+++ zapper-fix-staking-balance.js
@@ -11,41 +11,67 @@
 
 const sleep = async (ms) => new Promise((res, rej) => setTimeout(res, ms));
 
+const prettyNumber = (x) => {
+    return Number(x.toFixed(2))
+        .toString()
+        .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+};
+
 (async function () {
     "use strict";
 
+    const TEXT_ALT_COLOR = "#c7d2da";
+
     // fetch validator balance from custom api
-    const res = await fetch(
+    const balRes = await fetch(
         "https://base32.org/api/eth/validator-balance/90016445a797bb81f131146df66e094271f7df43e82603c51180a83c76f283060611967972f327b0b4566097f608dc8d"
     );
-    const { balance } = await res.json();
+    const { balance, price } = await balRes.json();
     const rewards = balance - 32;
 
-    // wait while loading spinner is present
+    // wait while loading spinner is pbalResent
     let exists = document.getElementsByClassName("lds-dual-ring");
     let timeout = 1;
     while (!exists) {
         await sleep(1);
         timeout *= 2;
     }
-    // we'll wait another 50ms just to allow things to finish rendering
-    await sleep(50);
+    // we'll wait a bit more just to allow things to finish rendering
+    await sleep(333);
 
     // fetching the current ether balance from zapper
     const balElement = document.querySelector('[data-testid="1"]');
     const currentBal = Number(balElement.textContent.replace(/[^\d.]/g, ""));
+    console.log("current balance: " + currentBal);
+    console.log("rewards: " + rewards);
 
     // increase relevant balances
     const totalBal = currentBal + rewards;
-    balElement.textContent = `Ξ ${totalBal.toFixed(2)}`;
-    const spans = document.getElementsByTagName("span");
-    for (const span of spans) {
-        // stupidly displays something besides 32 sometimes
-        if (
-            span.textContent.includes("Ξ 32") ||
-            span.textContent.includes("Ξ 31")
-        ) {
-            span.textContent = "Ξ " + (32 + rewards).toFixed(2);
+
+    console.log("total balance: " + totalBal);
+
+    balElement.textContent = `Ξ ${prettyNumber(totalBal)}`;
+
+    // add usd balance
+    const usdBalSpan = document.createElement("span");
+    usdBalSpan.textContent = `$${prettyNumber(totalBal * price)}`;
+    usdBalSpan.style.fontSize = "medium";
+    usdBalSpan.style.marginLeft = "0.77rem";
+    usdBalSpan.style.color = TEXT_ALT_COLOR;
+    balElement.appendChild(usdBalSpan);
+
+    const links = document.getElementsByTagName("a");
+    for (const link of links) {
+        if (link.href.includes("ethereum-staking")) {
+            // the one we're after has child nodes
+            if (link.hasChildNodes) {
+                const spans = link.getElementsByTagName("span");
+                for (const span of spans) {
+                    if (span.textContent.includes("Ξ")) {
+                        span.textContent = `Ξ ${prettyNumber(balance)}`;
+                    }
+                }
+            }
         }
     }
 })();
