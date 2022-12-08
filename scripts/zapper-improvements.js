680c79e50c28632d46a9bc61b4111262c4c2d13f
extrahash <ExtraHash@protonmail.com>
2022-12-07 20:10:22 -0700

    improvement

--- scripts/zapper-improvements.js
+++ scripts/zapper-improvements.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Fetches validator rewards from custom API and adds them to your zapper balance
 // @author       ExtraHash
-// @include        https://zapper.fi*
+// @invlude        https://zapper.fi*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zapper.fi
 // @grant        none
 // ==/UserScript==
@@ -13,6 +13,7 @@ let totalBal = 0;
 
 async function main() {
     addPriceSpanToHeader();
+    modifyBalances();
     listenToSocket();
 }
 
@@ -101,5 +102,85 @@ async function updateEthPrice(priceInfo) {
 }
 
 const sleep = async (ms) => new Promise((res) => setTimeout(res, ms));
+
+async function modifyBalances() {
+    "use strict";
+
+    // fetch validator balance from custom api
+    const balRes = await fetch(
+        "https://base32.org/api/eth/validator-balance/90016445a797bb81f131146df66e094271f7df43e82603c51180a83c76f283060611967972f327b0b4566097f608dc8d"
+    );
+    const { balance } = await balRes.json();
+    const rewards = balance - 32;
+
+    const unwrapRateRes = await fetch(
+        "https://api-public.sandbox.pro.coinbase.com/wrapped-assets/CBETH/conversion-rate"
+    );
+    const unwrapData = await unwrapRateRes.json();
+    const unwrapRate = unwrapData.amount;
+
+    console.log(unwrapRate);
+
+    // fetch eth price
+    const priceRes = await fetch("https://base32.org/api/eth/price");
+    const priceInfo = await priceRes.json();
+
+    console.log("fetched info", priceInfo);
+
+    await sleep(200);
+
+    // wait while loading spinner is pbalResent
+    let exists = document.getElementsByClassName("lds-dual-ring");
+    let timeout = 1;
+    while (exists.length === 1) {
+        await sleep(1);
+        timeout *= 2;
+        exists = document.getElementsByClassName("lds-dual-ring");
+    }
+
+    console.log("loaded");
+
+    // fetching the current ether balance from zapper
+    const balElement = document.querySelector('[data-testid="1"]');
+    const currentBal = Number(balElement.textContent.replace(/[^\d.]/g, ""));
+
+    console.log("current balance: " + currentBal);
+    console.log("rewards: " + rewards);
+
+    // increase relevant balances
+    totalBal = currentBal;
+
+    console.log("total balance: " + totalBal);
+    balElement.textContent = `Ξ ${prettyNumber(totalBal)}`;
+
+    // add usd balance
+    const usdBalSpan = document.createElement("span");
+
+    usdBalSpan.id = "usd-bal-span";
+    usdBalSpan.style.fontSize = "medium";
+    usdBalSpan.style.float = "right";
+    usdBalSpan.style.marginTop = "7px";
+    usdBalSpan.style.marginRight = "10px";
+
+    console.log(balElement, usdBalSpan);
+
+    balElement.appendChild(usdBalSpan);
+    setUsdBal(priceInfo);
+
+    const links = document.getElementsByTagName("a");
+    for (const link of links) {
+        if (link.href.includes("ethereum-staking")) {
+            // the one we're after has child nodes
+            if (link.hasChildNodes) {
+                const spans = link.getElementsByTagName("span");
+                for (const span of spans) {
+                    if (span.textContent.includes("Ξ")) {
+                        span.textContent = `Ξ ${prettyNumber(balance)}`;
+                    }
+                }
+            }
+        }
+    }
+}
 
 main();
